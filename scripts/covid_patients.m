b57cd7a511a354e7b7ff6867f6c952dbc18cb93e
Kohei Ikeda <tdn.suama@gmail.com>
2021-05-23 00:14:57 +0900

    ファイルパスを修正

--- scripts/covid_patients.m
+++ scripts/covid_patients.m
@@ -1,11 +1,5 @@
-%% テキスト ファイルからのデータのインポート
-% 次のテキスト ファイルからデータをインポートするスクリプト:
-%
-%    ファイル名: C:\work\covid19-data\200000_nagano_covid19_patients.csv
-%
-% MATLAB からの自動生成日: 2021/05/17 01:07:38
 %% ホームページからCSVファイルを取得
-websave('200000_nagano_covid19_patients.csv','https://www.pref.nagano.lg.jp/hoken-shippei/kenko/kenko/kansensho/joho/documents/200000_nagano_covid19_patients.csv');
+websave('../csv/200000_nagano_covid19_patients.csv','https://www.pref.nagano.lg.jp/hoken-shippei/kenko/kenko/kansensho/joho/documents/200000_nagano_covid19_patients.csv');
 
 %% インポート オプションの設定およびデータのインポート
 opts = delimitedTextImportOptions("NumVariables", 15);
@@ -28,7 +22,7 @@ opts = setvaropts(opts, ["Pref", "Municipalities", "OnsetDate", "Residence", "Ag
 opts = setvaropts(opts, "ConfirmedDate", "InputFormat", "yyyy/MM/dd");
 
 % データのインポート
-nagano_covid19_patients = readtable("C:\work\covid19-data\200000_nagano_covid19_patients.csv", opts);
+nagano_covid19_patients = readtable("../csv/200000_nagano_covid19_patients.csv", opts);
 
 
 %% 一時変数のクリア
--- scripts/getTestCount.m
+++ scripts/getTestCount.m
@@ -1,11 +1,5 @@
-%% テキスト ファイルからのデータのインポート
-% 次のテキスト ファイルからデータをインポートするスクリプト:
-%
-%    ファイル名: C:\work\covid19-data\200000_nagano_covid19_test_count.csv
-%
-% MATLAB からの自動生成日: 2021/05/17 01:24:42
 %% ホームページからCSVファイルを取得
-websave('200000_nagano_covid19_patients.csv','https://www.pref.nagano.lg.jp/hoken-shippei/kenko/kenko/kansensho/joho/documents/200000_nagano_covid19_patients.csv');
+websave('../csv/200000_nagano_covid19_test_count.csv','https://www.pref.nagano.lg.jp/hoken-shippei/kenko/kenko/kansensho/joho/documents/200000_nagano_covid19_test_count.csv');
 %% インポート オプションの設定およびデータのインポート
 opts = delimitedTextImportOptions("NumVariables", 8);
 
@@ -14,7 +8,7 @@ opts.DataLines = [3, Inf];
 opts.Delimiter = ",";
 
 % 列名と型の指定
-opts.VariableNames = ["Date", "LocalGovCode", "Pref", "Municipalities", "InspectionNum", "Remarks", "Positive", "Negative"];
+opts.VariableNames = ["InspectionDate", "LocalGovCode", "Pref", "Municipalities", "InspectionNum", "Remarks", "Positive", "Negative"];
 opts.VariableTypes = ["datetime", "double", "string", "string", "double", "string", "double", "double"];
 
 % ファイル レベルのプロパティを指定
@@ -24,10 +18,10 @@ opts.EmptyLineRule = "read";
 % 変数プロパティを指定
 opts = setvaropts(opts, ["Municipalities", "Remarks"], "WhitespaceRule", "preserve");
 opts = setvaropts(opts, ["Pref", "Municipalities", "Remarks"], "EmptyFieldRule", "auto");
-opts = setvaropts(opts, "Date", "InputFormat", "yyyy/MM/dd");
+opts = setvaropts(opts, "InspectionDate", "InputFormat", "yyyy/MM/dd");
 
 % データのインポート
-naganocovid19testcount = readtable("C:\work\covid19-data\200000_nagano_covid19_test_count.csv", opts);
+naganocovid19testcount = readtable("../csv/200000_nagano_covid19_test_count.csv", opts);
 
 
 %% 一時変数のクリア
