1d9f820ac1708fe239b9bc85da875c21d84182f7
kohei ikeda <tdn.suama@gmail.com>
2021-09-16 10:07:50 +0900

    Update covid_patients.m

--- scripts/covid_patients.m
+++ scripts/covid_patients.m
@@ -1,5 +1,5 @@
 %% ホームページからCSVファイルを取得
-websave('csv/200000_nagano_covid19_patients.csv','https://www.pref.nagano.lg.jp/hoken-shippei/kenko/kenko/kansensho/joho/documents/200000_nagano_covid19_patients.csv');
+% websave('csv/200000_nagano_covid19_patients.csv','https://www.pref.nagano.lg.jp/hoken-shippei/kenko/kenko/kansensho/joho/documents/200000_nagano_covid19_patients.csv');
 ver;
 %% インポート オプションの設定およびデータのインポート
 opts = delimitedTextImportOptions("NumVariables", 15);
@@ -22,9 +22,10 @@ opts = setvaropts(opts, ["Pref", "Municipalities", "OnsetDate", "Residence", "Ag
 opts = setvaropts(opts, "ConfirmedDate", "InputFormat", "yyyy/MM/dd");
 
 % データのインポート
-nagano_covid19_patients = readtable("csv/200000_nagano_covid19_patients.csv", opts);
+% nagano_covid19_patients = readtable("csv/200000_nagano_covid19_patients.csv", opts);
 
 % run simulink model simulation
+display('run simulation.');
 tic
 load_system('model/testmodel');
 sim('model/testmodel');
