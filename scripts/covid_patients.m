552ef18f90b5452ccf3d80be6d1fbff9c04c286e
kohei ikeda <tdn.suama@gmail.com>
2021-11-17 16:16:50 +0900

    Update covid_patients.m
    プラグインの個数を見たかった

--- scripts/covid_patients.m
+++ scripts/covid_patients.m
@@ -1,5 +1,6 @@
 %% ホームページからCSVファイルを取得
 % websave('csv/200000_nagano_covid19_patients.csv','https://www.pref.nagano.lg.jp/hoken-shippei/kenko/kenko/kansensho/joho/documents/200000_nagano_covid19_patients.csv');
+display(length(ver));
 ver;
 %% インポート オプションの設定およびデータのインポート
 opts = delimitedTextImportOptions("NumVariables", 15);
