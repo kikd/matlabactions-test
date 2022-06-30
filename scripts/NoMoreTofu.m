f90d33ef9c74e339942b71fc004e2007907fab83
kohei ikeda <tdn.suama@gmail.com>
2022-06-30 11:33:31 +0900

    豆腐撃退の実験

---
+++ scripts/NoMoreTofu.m
@@ -0,0 +1,19 @@
+% この関数ではNoto Fontが出てこない
+listfonts
+isjava = usejava('awt')
+% この非公開APIにを使うとNoto Fontが表示される。(ということは使える可能性がある？！)
+fontlist = com.mathworks.mwswing.FontUtils.getFontNames.toArray();
+fonts = cell(fontlist)
+f = figure();
+x = 0:pi/20:2*pi;
+y = sin(x);
+plot(x,y);
+t = text(pi,0,'\leftarrow テストsin(\pi)');
+% フォントを指定しないと、日本語テキストが豆腐になる。。。
+saveas(f, "test.png");
+% listfontsでは出てこないけど、日本語フォントは指定できるっぽい(なぜ？)
+t.FontName='Noto Serif CJK JP';
+saveas(f, "test2.png");
+
+%% 一時変数のクリア
+clear 
